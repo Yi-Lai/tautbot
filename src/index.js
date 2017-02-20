cf0a530f09fd48861315ce38d186c578d035167e
Yi Lai <yi_lai@outlook.com>
2017-02-20 10:18:10 -0500

    brought wolframalpha over

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const slackBotKey = process.env.slackkey;
 
 const apiAiService = apiai(apiAiAccessToken);
 
-var wolframalpha = require('./wolframalpha');
+var wolframalpha = "http://api.wolframalpha.com/v1/simple?appid=UX3QU2-76QKEYJJPG&i=";
 
 const sessionIds = new Map();
 
@@ -73,7 +73,21 @@ function isDefined(obj) {
 
     return obj != null;
 }
-wolframalpha();
+
+constroller.hears(['direct_mention'], function (bot, message) {
+var query = message.match[1];
+request(`wolframalpha${query}`, function(err, result) {
+        if (err) {
+            console.log("Error", err);
+        } else  if (result.body ==="Wolfram|Alpha did not understand your input") {
+            bot.replyWithTyping(message, ":sad_face:")
+        } else if (result.body === "No short answer available") {
+            bot.replyWithTyping(message, ":thinking_face:")
+        } else {
+            bot.replyWithTyping(message, result.body);
+        }
+})
+})
 
 controller.hears(['(.*)'], ['direct_message'], (bot, message) => {
     try {
