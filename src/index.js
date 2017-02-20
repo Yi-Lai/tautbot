124e3658a6e4f1b4416d0a0d48560ab477ced62b
Yi Lai <yi_lai@outlook.com>
2017-02-20 10:48:26 -0500

    fixing bug

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const slackBotKey = process.env.slackkey;
 
 const apiAiService = apiai(apiAiAccessToken);
 
-var wolframalpha = "http://api.wolframalpha.com/v1/simple?appid=UX3QU2-76QKEYJJPG&i=";
+var wolframalpha = `http://api.wolframalpha.com/v1/simple?appid=UX3QU2-76QKEYJJPG&i=${query}`;
 
 const sessionIds = new Map();
 
@@ -76,7 +76,7 @@ function isDefined(obj) {
 
 controller.hears(['direct_mention'], function (bot, message) {
 var query = message.match[1];
-request(wolframalpha`${query}`, function(err, result) {
+request(`wolframalpha`, function(err, result) {
         if (err) {
             console.log("Error", err);
         } else  if (result.body ==="Wolfram|Alpha did not understand your input") {
